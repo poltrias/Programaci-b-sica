d1be60ab4fc8d9a33ee39289d2e77f5d49ed47df
poltrias <jbelmonte970@gmail.com>
2022-11-04 17:38:08 +0100

    X

---
+++ Exercici1.2.py
@@ -0,0 +1,27 @@
+#EXERCICI 1 AMB INPUTS
+numero1=float(input("Inserta un numero decimal: "))
+numero2=float(input("Inserta un altre numero decimal: "))
+
+#resta
+print("la resta dels dos nombres es", numero1-numero2)
+
+#suma
+print("la suma dels dos nombres es", numero1+numero2)
+
+#potencia
+print("la potencia dels dos nombres es", numero1**numero2)
+
+#incrementa en 1
+numero1+=1
+
+#decrementa en 2
+numero1-=2
+
+#Modul
+print("el modul dels dos nombres es", numero1%numero2)
+
+#Divisio sencera
+print("la divisio sencera dels dos nombres es", numero1//numero2)
+
+#Divisio
+print("la divisio dels dos nombres es", numero1/numero2)

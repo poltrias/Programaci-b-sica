c55c03515f6d152f6faa516f3e4278a8594db213
poltrias <jbelmonte970@gmail.com>
2022-11-04 13:07:52 +0100

    X

---
+++ Exercici1.py
@@ -0,0 +1,26 @@
+numero1=9.5
+numero2=2.3
+
+#resta
+print(numero1-numero2)
+
+#suma
+print(numero1+numero2)
+
+#potencia
+print(numero1**numero2)
+
+#incrementa en 1
+numero1+=1
+
+#decrementa en 2
+numero1-=2
+
+#Modul
+print(numero1%numero2)
+
+#Divisio sencera
+print(numero1//numero2)
+
+#Divisio
+print(numero1/numero2)

c610898fd61f2eae3be50c1fca785bdd5f4ac4a5
poltrias <jbelmonte970@gmail.com>
2022-11-04 17:45:55 +0100

    X

---
+++ Exercici4.py
@@ -0,0 +1,15 @@
+#EXERCICI 4 AMB INPUTS
+nota=float(input("Quina nota tens: "))
+
+if nota<5:
+    print("Suspes")
+elif nota>=5 and nota<=6:
+    print("Be")
+elif nota>6 and nota<=8:
+    print("Notable")
+elif nota>8:
+    print("Excelent")
+elif nota<0 or nota>10:
+    print("Qualificacio incorrecta")
+else:
+    pass

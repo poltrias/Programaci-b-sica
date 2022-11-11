38b13dd215ce0977fe01804126a5192e4380a827
poltrias <jbelmonte970@gmail.com>
2022-11-11 17:19:10 +0100

    adeu

---
+++ Exercicis_bucles/Exercici4.py
@@ -0,0 +1,5 @@
+array=["Pol", "Marc", "Amaia", "Maria"]
+print(array[0], "te", len(array[0]), "lletres")
+print(array[1], "te", len(array[1]), "lletres")
+print(array[2], "te", len(array[2]), "lletres")
+print(array[3], "te", len(array[3]), "lletres")

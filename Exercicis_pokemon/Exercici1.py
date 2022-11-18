1ea07c6b7396824bef19cdd2cb64c56df0a0a785
poltrias <jbelmonte970@gmail.com>
2022-11-18 13:23:34 +0100

    create exercicis pokemon

---
+++ Exercicis_pokemon/Exercici1.py
@@ -0,0 +1,5 @@
+genere_entrenador=input("Introdueix el genere de l'entrenador: ")
+nom_entrenador=input("Introdueix el nom de l'entrenador: ")
+id_entrenador=int(input("Introdueix l'id de l'entrenador: "))
+info_entrenador={"genere": genere_entrenador, "nom":nom_entrenador, "id":id_entrenador}
+print("info sobre l'entrenador: ", info_entrenador)

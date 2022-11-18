add10638057f16f4694bc4ac903a2b1f5b2abc9f
poltrias <jbelmonte970@gmail.com>
2022-11-18 13:58:36 +0100

    create exercicis pokemon

---
+++ Exercicis_pokemon/Exercici2.py
@@ -0,0 +1,28 @@
+import pprint
+pokemon1= {"Nom" : "Lapras", "Tipus" : "Agua, hielo", "Atacs" : "Cascada, Cola ferrea", "numero_id" : 1,
+          "numero_pokedex" : 131, "Punts de vida" : 100, "atac" : 30, "Defensa" : 80, "Atac especial" : 40,
+          "Defensa especial" : 50, "habilitat" : "Absorbe agua", "Velocitat" : 55}
+
+pokemon2= {"Nom" : "Vileplume", "Tipus" : "Planta, veneno", "Atacs" : "Giga impacto, Tormenta floral", "numero_id" : 2,
+          "numero_pokedex" : 45, "Punts de vida" : 70, "atac" : 40, "Defensa" : 70, "Atac especial" : 45,
+          "Defensa especial" : 50, "habilitat" : "Clorofila", "Velocitat" : 55}
+
+pokemon3= {"Nom" : "Moltres", "Tipus" : "Fuego, Volador", "Atacs" : "Poder pasado, Asquas", "numero_id" : 3,
+          "numero_pokedex" : 146, "Punts de vida" : 90, "atac" : 55, "Defensa" : 50, "Atac especial" : 70,
+          "Defensa especial" : 30, "habilitat" : "Presion", "Velocitat" : 65}
+
+pokemon4= {"Nom" : "Alakazam", "Tipus" : "Psiquico", "Atacs" : "Juego sucio, Desarme", "numero_id" : 4,
+          "numero_pokedex" : 64, "Punts de vida" : 70, "atac" : 55, "Defensa" : 55, "Atac especial" : 55,
+          "Defensa especial" : 40, "habilitat" : "Sincronia", "Velocitat" : 50}
+
+pokemon5= {"Nom" : "Machamp", "Tipus" : "Lucha", "Atacs" : "Corpulencia, Sumision", "numero_id" : 5,
+          "numero_pokedex" : 67, "Punts de vida" : 75, "atac" : 80, "Defensa" : 50, "Atac especial" : 30,
+          "Defensa especial" : 35, "habilitat" : "Agallas", "Velocitat" : 65}
+
+pokemon6= {"Nom" : "Golem", "Tipus" : "Roca, Tierra", "Atacs" : "Lanzarocas, Terratemblor", "numero_id" : 6,
+          "numero_pokedex" : 75, "Punts de vida" : 95, "atac" : 45, "Defensa" : 80, "Atac especial" : 70,
+          "Defensa especial" : 40, "habilitat" : "Robustez", "Velocitat" : 30}
+
+equipo_pokemon=pokemon1, pokemon2, pokemon3, pokemon4, pokemon5, pokemon6
+pprint.pprint(equipo_pokemon)
+

6a833fc4597b5aee5e68cabcdf2858b04692b2cd
poltrias <jbelmonte970@gmail.com>
2023-01-20 09:13:08 +0100

    Update

--- Preparació_examen_final/Exercici4.py
+++ Preparació_examen_final/Exercici4.py
@@ -1,9 +1,9 @@
-def numerosCapicua():
-    l=[3,45,656,90,23,66]
+l = [3, 45, 656, 90, 23, 66]
+def numerosCapicua(l):
     for numero in l:
         numero=str(numero)
         if numero==numero[::-1]:
             print("El numero", numero, "es capicua")
         else:
             pass
-numerosCapicua()
+numerosCapicua(l)

fa2fb6daa1a92c93a9acba8d5534144a8d0f4f85
poltrias <jbelmonte970@gmail.com>
2023-01-20 09:12:44 +0100

    Update

--- Preparació_examen_final/Exercici3.py
+++ Preparació_examen_final/Exercici3.py
@@ -1,6 +1,6 @@
-def nombresParells():
-    l = [-2, 0, 3, 4, 5, 6, 7, 8]
+l = [-2, 0, 3, 4, 5, 6, 7, 8]
+def nombresParells(l):
     for numero in l:
         if numero % 2 == 0:
             print(numero)
-nombresParells()
+nombresParells(l)
